14aa64a3a482fca3833d439fbb1a74b2e3c1a445
Vasudev Sharma <vasudevsharma74@yahoo.com>
2021-02-28 18:49:10 +0530

    Fix materialized view name

--- query/adm_details.sql
+++ query/adm_details.sql
@@ -1,5 +1,5 @@
 DROP MATERIALIZED VIEW IF EXISTS adm_details CASCADE;
-CREATE MATERIALIZED VIEW pivoted_lab as
+CREATE MATERIALIZED VIEW adm_details as
 (
     select p.subject_id, p.gender, p.dob, p.dod, hadm_id, admittime, dischtime, admission_type, insurance, marital_status, ethnicity, hospital_expire_flag, has_chartevents_data
     from admissions adm
